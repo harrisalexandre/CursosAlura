3ea0bbe626b58451283374530ee0b7a1422e3ae2
HarrisAlexandre <harrisalexandre308@gmail.com>
2021-07-27 14:19:30 -0300

    att

---
+++ BACKEND I Java e SpringBoot/eclipse/sintaxe-java-variaveis-e-fluxo/src/TestaConversao.java
@@ -0,0 +1,23 @@
+
+public class TestaConversao {
+
+	public static void main(String[] args) {
+		
+		double salario = 1270.50;
+		
+		float pontoFlutuante = 3.14F;
+		
+		int valor = (int) salario;
+		System.out.println(valor);
+		
+		long numeroGrande = 951951931951951926L;
+		short valorPequeno = 1515;
+		byte b = 127;
+		
+		double valor1 = 0.1;
+		double valor2 = 0.2;
+		double total = valor1 + valor2;
+		System.out.println(total);
+	
+	}
+}
