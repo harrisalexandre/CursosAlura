a7608ff5c1ba5c20368c6c9f8d28cbf23668f275
Harris Alexandre <harrisalexandre308@gmail.com>
2021-08-06 17:04:09 -0300

    aa

--- MOBILE React Native e Flutter/bytebank/lib/main.dart
+++ MOBILE React Native e Flutter/bytebank/lib/main.dart
@@ -1,30 +1,16 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
-      home: Scaffold(
-        body: Column(
-          children: <Widget>[
-            Card(
-              child: ListTile(
-                leading: Icon(Icons.monetization_on),
-                title: Text('100.0'),
-                subtitle: Text('1000'),
-              ),
-            ),
-            Card(
-              child: ListTile(
-                leading: Icon(Icons.monetization_on),
-                title: Text('200.0'),
-                subtitle: Text('1000'),
-              ),
-            ),
-          ],
-        ),
-        appBar: AppBar(
-          title: Text('Transferências'),
-        ),
-        floatingActionButton: FloatingActionButton(
-          child: Icon(Icons.add),
-        ),
-      ),
-    ));
+void main() => runApp(
+      Column(
+        children: <Widget>[
+          Text('Deliver features faster', textDirection: TextDirection.ltr),
+          Text('Craft beautiful UIs', textDirection: TextDirection.ltr),
+          Expanded(
+            child: FittedBox(
+              fit: BoxFit.contain, // otherwise the logo will be tiny
+              child: const FlutterLogo(),
+            ), //fittedBox
+          ),
+        ], //<Widget>[]
+      ), //Column
+    );
