4a2b3535d865d894bcf42c710038f8b00c027bdf
Harris Alexandre <harrisalexandre308@gmail.com>
2021-08-06 00:17:17 -0300

    att

--- MOBILE React Native e Flutter/cozinhando_em_casa/lib/telas/home/home.dart
+++ MOBILE React Native e Flutter/cozinhando_em_casa/lib/telas/home/home.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:cozinhando_casa2/modelos/receita.dart';
 import 'dart:convert';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => new HomeState();
+}
+
+class HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return _construirHome();
@@ -10,45 +16,66 @@ class Home extends StatelessWidget {
   Widget _construirHome() {
     return Scaffold(
         body: SizedBox(
-            height: 270,
-            child: _construirCard()
-        ),
-        appBar: _construirAppBar('Cozinhando em Casa')
-    );
+            child: FutureBuilder(
+          future:
+              DefaultAssetBundle.of(context).loadString('assets/receitas.json'),
+          builder: (context, snapshot) {
+            List<dynamic> receitas = json.decode(snapshot.data.toString());
+
+            return ListView.builder(
+              itemBuilder: (BuildContext context, int index) {
+                Receita receita = Receita.fromJson(receitas[index]);
+                return _construirCard(receita.foto, receita.titulo);
+              },
+              itemCount: receitas == null ? 0 : receitas.length,
+            );
+          },
+        )),
+        appBar: _construirAppBar('Cozinhando em Casa'));
   }
 
-  Widget _construirCard() {
+  Widget _construirCard(foto, titulo) {
     return Card(
         margin: EdgeInsets.all(16),
         child: Column(
           children: <Widget>[
             Stack(
               children: <Widget>[
-                _construirImagemCard('https://receitas.carolecrema.com.br/wp-content/uploads/2018/10/shutterstock_662262010-848x477.jpg'),
-                _construirTituloCard('Bolo de Milho')
+                _construirImagemCard(foto),
+                _construirImagemGradienteCard(),
+                _construirTituloCard(titulo)
               ],
             ),
           ],
-        )
-    );
+        ));
   }
 
   Widget _construirImagemCard(String imagem) {
-    return Image.network(imagem,
-        fit: BoxFit.fill,
-        height: 238);
+    return Image.asset(imagem, fit: BoxFit.fill, height: 238);
   }
 
   Widget _construirTituloCard(String titulo) {
     return Positioned(
         bottom: 10.0,
         left: 10.0,
-        child: Text(titulo, style: TextStyle(fontSize: 20))
-    );
+        child:
+            Text(titulo, style: TextStyle(color: Colors.white, fontSize: 20)));
+  }
+
+  Widget _construirImagemGradienteCard() {
+    return Container(
+        height: 238,
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+                begin: FractionalOffset.topCenter,
+                end: FractionalOffset.bottomCenter,
+                colors: [
+              Colors.transparent,
+              Colors.deepOrange.withOpacity(0.7)
+            ])));
   }
 
   Widget _construirAppBar(String titulo) {
     return AppBar(title: Text(titulo));
   }
-
 }
--- MOBILE React Native e Flutter/cozinhando_em_casa/test/widget_test.dart
+++ MOBILE React Native e Flutter/cozinhando_em_casa/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:cozinhando_em_casa/main.dart';
+import 'package:cozinhando_casa2/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
