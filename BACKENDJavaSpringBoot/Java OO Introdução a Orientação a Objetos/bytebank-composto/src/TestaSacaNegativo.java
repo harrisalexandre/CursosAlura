7bea6822266a0852bdeababc4531f7f55300633f
Guilherme Berta <gui.m.berta16@gmail.com>
2021-08-11 03:20:08 -0300

    Java OO encapsulamento

---
+++ BACKENDJavaSpringBoot/Java OO Introdução a Orientação a Objetos/bytebank-composto/src/TestaSacaNegativo.java
@@ -0,0 +1,15 @@
+
+public class TestaSacaNegativo {
+	public static void main(String[] args) {
+		Conta conta = new Conta();
+		conta.deposita(100);
+		System.out.println(conta.saca(200));
+		
+		System.out.println(conta.pegaSaldo());
+		
+		// nao pode, deixei pra testar 
+		// resulta = The field Conta.saldo is not visible
+		// conta.saldo = conta.saldo - 101;
+		// System.out.println(conta.saldo);
+	}
+}
