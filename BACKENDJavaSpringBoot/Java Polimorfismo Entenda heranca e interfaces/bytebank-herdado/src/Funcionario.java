6979cf2a0345c63a016d25c27f45ffd718a9a208
HarrisAlexandre <harrisalexandre308@gmail.com>
2021-08-14 17:38:01 -0300

    Java Polimorfismo

--- BACKENDJavaSpringBoot/Java Polimorfismo Entenda heranca e interfaces/bytebank-herdado/src/Funcionario.java
+++ BACKENDJavaSpringBoot/Java Polimorfismo Entenda heranca e interfaces/bytebank-herdado/src/Funcionario.java
@@ -1,39 +1,35 @@
-
 public class Funcionario {
-	private String nome;
-	private String cpf;
-	private double salario;
-	
-	public Funcionario() {
-		
-	}
 
-	public double getBonificacao() {
-		return this.salario * 0.1;
-	}
-	
-	public String getNome() {
-		return nome;
-	}
+    private String nome;
+    private String cpf;
+    private double salario;
 
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
+    public double getBonificacao() {
+        return this.salario * 0.05;
+    }
 
-	public String getCpf() {
-		return cpf;
-	}
+    public String getNome() {
+        return nome;
+    }
 
-	public void setCpf(String cpf) {
-		this.cpf = cpf;
-	}
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 
-	public double getSalario() {
-		return salario;
-	}
+    public String getCpf() {
+        return cpf;
+    }
 
-	public void setSalario(double salario) {
-		this.salario = salario;
-	}
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
 
 }
---
+++ BACKENDJavaSpringBoot/Java Polimorfismo Entenda heranca e interfaces/bytebank-herdado/src/GerenteTeste.java
@@ -0,0 +1,9 @@
+
+public class GerenteTeste {
+	main static void main(Strig[ ] args) {
+		Gerente g1 = new Gerente();
+		g1.setNome("marco");
+		g1.setCPF("777999444-57")
+		g1.setSalario(1230);
+	}
+}
