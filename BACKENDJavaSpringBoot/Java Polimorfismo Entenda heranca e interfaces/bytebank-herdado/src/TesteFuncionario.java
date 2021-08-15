cd86cecb8eaf080096406b312771160d4f87bfb7
HarrisAlexandre <harrisalexandre308@gmail.com>
2021-08-15 15:33:43 -0300

    Praticando herancas e interfaces

--- BACKENDJavaSpringBoot/Java Polimorfismo Entenda heranca e interfaces/bytebank-herdado/src/TesteFuncionario.java
+++ BACKENDJavaSpringBoot/Java Polimorfismo Entenda heranca e interfaces/bytebank-herdado/src/TesteFuncionario.java
@@ -1,14 +1,14 @@
 
 public class TesteFuncionario {
+
 	public static void main(String[] args) {
-		Funcionario nico = new Funcionario();
+		Gerente nico = new Gerente();
 		nico.setNome("Nico Steppat");
-		nico.setCpf("333444555-66");
-		nico.setSalario(2600.0);
-		
+		nico.setCpf("223355646-9");
+		nico.setSalario(2600.00);
+
 		System.out.println(nico.getNome());
 		System.out.println(nico.getBonificacao());
-		
-		
 	}
+
 }
