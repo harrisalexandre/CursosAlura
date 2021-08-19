7c91a60502c17884eb7c8cddf0daf0920bd1cacf
HarrisAlexandre <harrisalexandre308@gmail.com>
2021-08-19 15:58:21 -0300

    Hierarquia de exceções

--- BACKENDJavaSpringBoot/Java Exceções Aprenda a criar lançar e controlar exceções/java-pilha/src/FluxoComTratamento.java
+++ BACKENDJavaSpringBoot/Java Exceções Aprenda a criar lançar e controlar exceções/java-pilha/src/FluxoComTratamento.java
@@ -1,5 +1,5 @@
-public class Fluxo {
 
+public class FluxoComTratamento {
 	public static void main(String[] args) {
 		System.out.println("Ini do main");
 		metodo1();
@@ -22,7 +22,13 @@ public class Fluxo {
 
 	private static void metodo2() {
 		System.out.println("Ini do metodo2");
-		throw new MinhaExcecao("deu muito errado");
-//			System.out.println("Fim do metodo2");
+		for (int i = 1; i <= 5; i++) {
+			System.out.println(i);
+			int a = i / 0;
+			Conta c = null;
+			c.deposita();
+
+		}
+		System.out.println("Fim do metodo2");
 	}
 }
