85d58802ec6f00840d81365f850f10e38d04f207
HarrisAlexandre <harrisalexandre308@gmail.com>
2021-08-20 03:16:50 -0300

    JAVA JDBC IMPORT br.com.harris.jdbc

--- BACKENDJavaSpringBoot/Java e JDBC Trabalhando com um banco de dados/loja-virtual-repository/src/br/com/harris/jdbc/TestaInsercaoComParametro.java
+++ BACKENDJavaSpringBoot/Java e JDBC Trabalhando com um banco de dados/loja-virtual-repository/src/br/com/harris/jdbc/TestaInsercaoComParametro.java
@@ -9,26 +9,32 @@ public class TestaInsercaoComParametro {
 
 	public static void main(String[] args) throws SQLException {
 
-		String nome = "Geladeira";
-		String descricao = "Branca";
 		ConnectionFactory factory = new ConnectionFactory();
 		Connection connection = factory.recuperaConexao();
-
+		connection.setAutoCommit(false);
 		PreparedStatement stm = connection.prepareStatement("INSERT INTO PRODUTO (nome, descricao) VALUES (?, ?)",
 				Statement.RETURN_GENERATED_KEYS);
 
+		adicionarVariavel("motoG", "moto oito", stm);
+		adicionarVariavel("iphone", "6s", stm);
+	}
+
+	private static void adicionarVariavel(String nome, String descricao, PreparedStatement stm) throws SQLException {
 		stm.setString(1, nome);
 		stm.setString(2, descricao);
 
+		if(nome.equals("iphone")); {
+			throw new RuntimeException("nao foi possivel add produto");
+	}
 		stm.execute();
 
 		ResultSet rst = stm.getGeneratedKeys();
-		while(rst.next()) {
+		while (rst.next())
+			;
+		{
 			Integer id = rst.getInt(1);
-			System.out.println("O id criado foi: " + id);
+			System.out.println("o id criado foi" + id);
 		}
 		rst.close();
-		stm.close();
-		connection.close();
 	}
 }
