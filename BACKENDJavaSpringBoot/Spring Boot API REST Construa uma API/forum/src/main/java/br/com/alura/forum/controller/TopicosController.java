bcbd657761fe01ca3af12d662d5aba865af6da5c
HarrisAlexandre <harrisalexandre308@gmail.com>
2021-08-30 04:01:13 -0300

    Java Springboot

--- BACKENDJavaSpringBoot/Spring Boot API REST Construa uma API/forum/src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ BACKENDJavaSpringBoot/Spring Boot API REST Construa uma API/forum/src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -3,21 +3,20 @@ package br.com.alura.forum.controller;
 import java.util.Arrays;
 import java.util.List;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
+import br.com.alura.forum.controller.dto.TopicoDto;
 import br.com.alura.forum.modelo.Curso;
 import br.com.alura.forum.modelo.Topico;
 
-@Controller
+@RestController
 public class TopicosController {
 
 	@RequestMapping("/topicos")
-	@ResponseBody
-	public List<Topico> lista() {
-		Topico topico = new Topico("Duvida", "duvida com spring", new Curso("Spring", "programacao"));
+	public List<TopicoDto> lista() {
+		Topico topico = new Topico("Duvida", "Dúvida com spring", new Curso("Spring", "programacao"));
 
-		return Arrays.asList(topico, topico, topico);
+		return TopicoDto.converter(Arrays.asList(topico, topico, topico));
 	}
 }
