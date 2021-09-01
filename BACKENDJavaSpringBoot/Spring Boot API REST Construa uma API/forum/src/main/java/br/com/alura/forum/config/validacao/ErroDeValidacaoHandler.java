82aa10d0f89d58d7a77c67fc6092d5c6c8beb3e2
HarrisAlexandre <harrisalexandre308@gmail.com>
2021-09-01 07:19:57 -0300

    Conclusao JAVA SPRINGBOOT

--- BACKENDJavaSpringBoot/Spring Boot API REST Construa uma API/forum/src/main/java/br/com/alura/forum/config/validacao/ErroDeValidacaoHandler.java
+++ BACKENDJavaSpringBoot/Spring Boot API REST Construa uma API/forum/src/main/java/br/com/alura/forum/config/validacao/ErroDeValidacaoHandler.java
@@ -22,7 +22,7 @@ public class ErroDeValidacaoHandler {
         @ResponseStatus(code = HttpStatus.BAD_REQUEST)
         @ExceptionHandler(MethodArgumentNotValidException.class)
         public List<ErroDeFormularioDto> handle(MethodArgumentNotValidException exception) {
-        List<ErroDeFormularioDto> dto = new ArrayList<>();
+        	List<ErroDeFormularioDto> dto = new ArrayList<>();
         
 
         List<FieldError> fieldErrors = exception.getBindingResult().getFieldErrors();
--- BACKENDJavaSpringBoot/Spring Boot API REST Construa uma API/forum/src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ BACKENDJavaSpringBoot/Spring Boot API REST Construa uma API/forum/src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -2,20 +2,26 @@ package br.com.alura.forum.controller;
 
 import java.net.URI;
 import java.util.List;
+import java.util.Optional;
 
+import javax.transaction.Transactional;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import br.com.alura.forum.controller.dto.DetalhesTopicoDto;
 import br.com.alura.forum.controller.dto.TopicoDto;
+import br.com.alura.forum.controller.form.AtualizacaoTopicoForm;
 import br.com.alura.forum.controller.form.TopicoForm;
 import br.com.alura.forum.controller.repository.CursoRepository;
 import br.com.alura.forum.modelo.Topico;
@@ -30,6 +36,7 @@ public class TopicosController {
 	@Autowired
 	private CursoRepository cursoRepository;
 
+	// listar
 	@GetMapping
 	public List<TopicoDto> lista(String nomeCurso) {
 		if (nomeCurso == null) {
@@ -41,18 +48,48 @@ public class TopicosController {
 		}
 	}
 
+	// Cadastrar
 	@PostMapping
 	public ResponseEntity<TopicoDto> cadastrar(@RequestBody @Valid TopicoForm form, UriComponentsBuilder uriBuilder) {
 		Topico topico = form.converter(cursoRepository);
 		topicoRepository.save(topico);
-		
+
 		URI uri = uriBuilder.path("/topicos/{id}").buildAndExpand(topico.getId()).toUri();
 		return ResponseEntity.created(uri).body(new TopicoDto(topico));
 	}
-	
+
+	// detalhar
 	@GetMapping("/{id}")
-	public TopicoDto detalhar(@PathVariable Long id) {
-		Topico topico = topicoRepository.getOne(id);
-		return new TopicoDto(topico);
+	public ResponseEntity<DetalhesTopicoDto> detalhar(@PathVariable Long id) {
+		Optional<Topico> topico = topicoRepository.findById(id);
+		if (topico.isPresent()) {
+			return ResponseEntity.ok(new DetalhesTopicoDto(topico.get()));
+		}
+		return ResponseEntity.notFound().build();
 	}
+
+	// atualizar
+	@PutMapping("/{id}")
+	@Transactional
+	public ResponseEntity<TopicoDto> atualizar(@PathVariable Long id, @RequestBody @Valid AtualizacaoTopicoForm form) {
+		Optional<Topico> optional = topicoRepository.findById(id);
+		if (optional.isPresent()) {
+			Topico topico = form.atualizar(id, topicoRepository);
+			return ResponseEntity.ok(new TopicoDto(topico));
+		}
+		return ResponseEntity.notFound().build();
+	}
+
+	// excluir/remover
+	@DeleteMapping("/{id}")
+	@Transactional
+	public ResponseEntity<?> remover(@PathVariable Long id) {
+		Optional<Topico> optional = topicoRepository.findById(id);
+		if (optional.isPresent()) {
+			topicoRepository.deleteById(id);
+			return ResponseEntity.ok().build();
+		}
+		return ResponseEntity.notFound().build();
+	}
+
 }
